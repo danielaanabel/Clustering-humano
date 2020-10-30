eee9bf4819050887fe2bb43e62b1d7596bbdc14c
Danielaanabel <danielaanabel2011@gmail.com>
2020-10-30 04:36:36 -0300

    agregue otra forma para agregar la arista

--- TrabajoPractico2/src/TP2/AGM.java
+++ TrabajoPractico2/src/TP2/AGM.java
@@ -1,8 +1,7 @@
 package TP2;
 
 import java.util.ArrayList;
-// otra implementacion de prim es tener todos los vertices por visitar en un array
-// y cada vez que visito un vertice lo quito y lo agrego a los ya marcados
+
 
 
 
@@ -13,33 +12,43 @@ public class AGM {
 		GrafoConPesos agm=new GrafoConPesos(g.tamano());
 
 		ArrayList<Integer> marcados=new ArrayList<Integer>();
-		marcados.add(0);//agrego el vertice inicial
+		marcados.add(0);//agrego el vertice inicial arbitrario
 
 		ArrayList<Integer> verticesG=new ArrayList<Integer>();//vertices aun no visitados
 		for(Integer j=0;j<g.tamano();j++)
 		{
 			verticesG.add(j);
 		}
-		verticesG.remove(0);
+		verticesG.remove(0);//elimino el vertice inicial
 
-		Integer [][] aristas=new Integer[1][3]; 	
+//		Integer [][] aristas=new Integer[1][3];
+		Arista arista=new Arista();
 
 		int i=0;
 		while(i<g.tamano()-1){
-			int minimo=0xfff;
+			int minimo=0xff;
 			for(int v=0;v<marcados.size();v++){
 				for(int n=0;n<verticesG.size();n++){
+					System.out.print(marcados.get(v));
+					System.out.println(verticesG.get(n));
 					if(g.obtenerPesoArista(marcados.get(v), verticesG.get(n))<minimo) {
 						minimo=g.obtenerPesoArista(marcados.get(v), verticesG.get(n));
-						aristas[0][0]=marcados.get(v);
-						aristas[0][1]=verticesG.get(n);
-						aristas[0][2]=minimo;
+						arista.setVerticeA(marcados.get(v));
+						arista.setVerticeB(verticesG.get(n));
+						arista.setPeso(minimo);
+//						aristas[0][0]=marcados.get(v);
+//						aristas[0][1]=verticesG.get(n);
+//						aristas[0][2]=minimo;
 					}
 				}		
 			}
-			marcados.add(aristas[0][1]);
-			verticesG.remove(aristas[0][1]);
-			agm.agregarAristaConPeso(aristas[0][0], aristas[0][1], aristas[0][2]);
+			Integer b=arista.getVerticeB();
+			marcados.add(b);
+			verticesG.remove(b);
+			agm.agregarAristaConPeso(arista.getVerticeA(), arista.getVerticeB(), arista.getPeso());
+//			marcados.add(aristas[0][1]);
+//			verticesG.remove(aristas[0][1]);
+//			agm.agregarAristaConPeso(aristas[0][0], aristas[0][1], aristas[0][2]);
 			i++;
 		}
 
@@ -64,7 +73,6 @@ public class AGM {
 		grafo.imprimiGrafo();
 		System.out.println();
 		AGM.generarArbolMinimo(grafo).imprimiGrafo();
-
 	}
 
 
---
+++ TrabajoPractico2/src/TP2/Arista.java
@@ -0,0 +1,28 @@
+package TP2;
+
+public class Arista {
+	private int verticeA;
+	private int verticeB;
+	private int peso;
+	
+	public int getVerticeA() {
+		return verticeA;
+	}
+	public void setVerticeA(int verticeA) {
+		this.verticeA = verticeA;
+	}
+	public int getVerticeB() {
+		return verticeB;
+	}
+	public void setVerticeB(int verticeB) {
+		this.verticeB = verticeB;
+	}
+	public int getPeso() {
+		return peso;
+	}
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+	
+	
+}
